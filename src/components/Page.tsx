5ca91b90c96b4422d682eb972db5b765a6251747
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2022-09-11 15:14:49 -0300

    resolving problems with images

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -97,7 +97,7 @@ export default function () {
                                     <span></span>
                                 </span>
                             </div>
-                            <img src={fivImage} alt=""></img>
+                            <img src={fivImage} alt="" className="fivImg"></img>
                         </a>
                     </li>
                 </ul>
